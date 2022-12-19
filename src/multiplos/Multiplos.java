6af09088910219f3f89d3a934cbcc06a35ef7132
Daniel0p <dani.hiver@gmail.com>
2022-12-19 10:40:20 -0600

    Agregando el proyecto

---
+++ src/multiplos/Multiplos.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package multiplos;
+
+import java.util.ArrayList;
+import java.util.List;
+/**
+ *
+ * @author 1er proyecto Danny y Amix
+ */
+public class Multiplos {
+    public static void main(String[] args) {
+        List<Integer> multiplos = new ArrayList();
+        int num1=3, num2=5, numi, sum=0, max=1000;
+        
+        for(numi=1; numi<max; numi++)
+        {
+            if ((numi%num1==0) || numi%num2==0){
+            //System.out.println("el numero " + numi + " es primo de 3");
+            sum += numi;
+            multiplos.add(numi);        
+         }
+        else{
+            //System.out.println("El numero " + numi + " no es primo de 3");
+            }
+        }
+         System.out.println("Los números multiplos son " + multiplos);
+         System.out.println("El resultado es "+ sum);
+    }
+}

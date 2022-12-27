542118c4d118d6ee95abfebbc628fc512856f05c
Daniel0p <dani.hiver@gmail.com>
2022-12-26 20:07:00 -0600

    cambios guardados

--- src/multiplos/Multiplos.java
+++ src/multiplos/Multiplos.java
@@ -12,11 +12,10 @@ import java.util.List;
  */
 public class Multiplos {
     public static void main(String[] args) 
-    {
-           
+    {   
         List<Integer> multiplos = new ArrayList();
         int num1=3, num2=5, numi, sum=0, max=1000;
-        for(numi=1; numi<max; numi++)
+        for(numi=1; numi<max; numi++)  
         {
             if ((numi%num1==0) || numi%num2==0){
             //System.out.println("el numero " + numi + " es primo de 3");
@@ -31,33 +30,5 @@ public class Multiplos {
          System.out.println("El resultado es "+ sum);
          System.out.println("Sistema editado v2");
          System.out.println("Se agrega nueva rama euler 007");    
-    }
-    
-    public void primos(){
-    
-          
-        ArrayList<Integer> primosLista= new ArrayList();
-        int num, den, lim=110000;
-        boolean primo;
-       
-        for (num=2; num<=lim; num++){
-                primo=true;
-            for (den=num-1; den>1; den--)
-            {
-                if (num%den==0)
-                {
-                    primo=false;
-                }
-            }
-           if(primo)
-           {
-            primosLista.add(num);
-           }
-        }
-        //System.out.println("La cantidad de números primos es:" + primosLista);
-        System.out.println("La cantidad de números primos es:" + primosLista.size());
-        System.out.println("El 10001ro primo de la lista es el " + primosLista.get(10000));
-        
-    
     }
 }

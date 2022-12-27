b626f24e2bc5cf90f259b3260445fe5532ac0f54
Daniel0p <dani.hiver@gmail.com>
2022-12-26 19:58:47 -0600

    cambio de codigo para numeros primos

--- src/multiplos/Multiplos.java
+++ src/multiplos/Multiplos.java
@@ -11,10 +11,11 @@ import java.util.List;
  * @author 1er proyecto Danny y Amix
  */
 public class Multiplos {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
+           
         List<Integer> multiplos = new ArrayList();
         int num1=3, num2=5, numi, sum=0, max=1000;
-        
         for(numi=1; numi<max; numi++)
         {
             if ((numi%num1==0) || numi%num2==0){
@@ -29,7 +30,34 @@ public class Multiplos {
          System.out.println("Los números multiplos son " + multiplos);
          System.out.println("El resultado es "+ sum);
          System.out.println("Sistema editado v2");
-         
-         System.out.println("Se agrega nueva rama euler 007");
+         System.out.println("Se agrega nueva rama euler 007");    
+    }
+    
+    public void primos(){
+    
+          
+        ArrayList<Integer> primosLista= new ArrayList();
+        int num, den, lim=110000;
+        boolean primo;
+       
+        for (num=2; num<=lim; num++){
+                primo=true;
+            for (den=num-1; den>1; den--)
+            {
+                if (num%den==0)
+                {
+                    primo=false;
+                }
+            }
+           if(primo)
+           {
+            primosLista.add(num);
+           }
+        }
+        //System.out.println("La cantidad de números primos es:" + primosLista);
+        System.out.println("La cantidad de números primos es:" + primosLista.size());
+        System.out.println("El 10001ro primo de la lista es el " + primosLista.get(10000));
+        
+    
     }
 }
